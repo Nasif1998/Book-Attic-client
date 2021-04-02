df00baddc9d35f4c4a4cc00c234f22f5319138e2
Nasif123 <nasif.reza@northsouth.edu>
2021-04-02 12:14:40 +0600

    price

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const OrderDetails = (props) => {
-    const { bookName, authorName, orderTime, _id } = props.orderDetails;
+    const { bookName, authorName, orderTime, _id, price } = props.orderDetails;
     return (
         <div style={{textAlign: 'center', marginBottom: '20px'}}>
 
@@ -9,6 +9,7 @@ const OrderDetails = (props) => {
                 <summary>Order of {bookName}</summary>
                 <p>Order ID: {_id}</p>
                 <p>Author Name: {authorName}</p>
+                <p>{price}</p>
                 <p>Time: {orderTime}</p>
             </details>
         </div>
